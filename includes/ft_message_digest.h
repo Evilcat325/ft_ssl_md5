233aa22e73e616c413e295cfd3b20698d816dfb0
Sen Li <ls.learnshare@gmail.com>
2019-10-29 15:59:32 -0700

    digest component

--- includes/ft_message_digest.h
+++ includes/ft_message_digest.h
@@ -1,20 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_md5.h                                           :+:      :+:    :+:   */
+/*   ft_message_digest.h                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 15:49:39 by seli              #+#    #+#             */
-/*   Updated: 2019/10/28 15:51:20 by seli             ###   ########.fr       */
+/*   Updated: 2019/10/29 15:58:38 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MD5_H
-# define FT_MD5_H
+#ifndef FT_MESSAGE_DIGEST_H
+# define FT_MESSAGE_DIGEST_H
 
-# include "ft_ssl.h"
+# include <stdio.h>
+# include <string.h>
+# include "ft_struct.h"
 
 char	*ft_md5(char* msg);
+char	*ft_sha(char* msg);
+
+t_dgst_t parse_dgst(t_parser_state* state, int ac, char* av[]);
 
 #endif
