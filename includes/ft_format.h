96ddc8e63ec55f966a3e643e6def3c42631d7f72
Sen Li <ls.learnshare@gmail.com>
2019-10-29 15:59:47 -0700

    formator

---
+++ includes/ft_format.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_format.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/29 15:03:01 by seli              #+#    #+#             */
+/*   Updated: 2019/10/29 15:12:33 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FORMAT_H
+# define FT_FORMAT_H
+
+# include <stdlib.h>
+# include "ft_struct.h"
+
+void	print_hash(t_parser_state *state, char *hash);
+
+#endif
