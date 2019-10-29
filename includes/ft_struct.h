cc74ff88543ceaddffef1a2694fb97c48fe4276f
Sen Li <ls.learnshare@gmail.com>
2019-10-29 15:59:55 -0700

    header refine

--- includes/ft_struct.h
+++ includes/ft_struct.h
@@ -1,25 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ssl.h                                           :+:      :+:    :+:   */
+/*   ft_struct.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/22 16:34:40 by seli              #+#    #+#             */
-/*   Updated: 2019/10/29 15:53:12 by seli             ###   ########.fr       */
+/*   Created: 2019/10/29 15:02:13 by seli              #+#    #+#             */
+/*   Updated: 2019/10/29 15:12:02 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_SSL_H
-# define FT_SSL_H
+#ifndef FT_STRUCT_H
+# define FT_STRUCT_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+typedef char	*(*t_dgst_t)(char *msg);
 
-#include "ft_struct.h"
-#include "ft_parser.h"
-#include "ft_format.h"
-#include "ft_message_digest.h"
+typedef struct		s_parser_state
+{
+	int						i_ac;
+	int						ac;
+	char					**av;
+	t_dgst_t				dgst_func;
+	char					*err;
+
+	unsigned int	flag_r;
+	unsigned int	flag_p;
+	unsigned int	flag_q;
+	unsigned int	flag_s;
+}					t_parser_state;
 
 #endif
